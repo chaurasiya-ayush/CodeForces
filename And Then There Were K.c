6b3be86257bd667f71865bd0d2e97d95e2c52dd8
Ayush-Chaurasiya <chaurasiyaayush2006@gmail.com>
2025-03-02 14:17:46 +0530

    And Then There Were K

--- And Then There Were K.c
+++ And Then There Were K.c
@@ -6,18 +6,44 @@ int main()
   int n;
   scanf("%d",&n);
   for(int i=1;i<=n;i++){
-      int x;
+      int x,c=1;
       scanf("%d",&x);
-      int c = x;
-      int j=1;
-      int k;
-      while(c!=0){
-          c = c&(x-j);
-          k=x-j;
-          j++;
-      }
-      printf("%d\n",k);
+        while(x>c){
+            c = 2*c;
+        }
+        if(c==x){
+            printf("%d\n",c-1);
+        }else{
+            
+     printf("%d\n",(c/2)-1);
+        }
+     
   }
 
     return 0;
 }
+
+
+// Not an optimise approach
+// #include <stdio.h>
+
+// int main()
+// {
+//   int n;
+//   scanf("%d",&n);
+//   for(int i=1;i<=n;i++){
+//       int x;
+//       scanf("%d",&x);
+//       int c = x;
+//       int j=1;
+//       int k;
+//       while(c!=0){
+//           c = c&(x-j);
+//           k=x-j;
+//           j++;
+//       }
+//       printf("%d\n",k);
+//   }
+
+//     return 0;
+// }
