a40e4c6b702baf17cc5a4a27f82f055a195d90d7
Ayush-Chaurasiya <chaurasiyaayush2006@gmail.com>
2025-05-10 23:49:08 +0530

    Petya and String

--- petyaStirng.c
+++ petyaStirng.c
@@ -1,34 +1,21 @@
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
 int main() {
-    int sum=0,sum2=0;
-    char name[100];
-    fgets(name,sizeof(name),stdin);
-    char name2[100];
-      fgets(name2,sizeof(name2),stdin);
-     for(int i=0;i<=strlen(name)-2;i++){
-        if(name[i]<97){
-            name[i] = name[i]+32;
-        } 
-        if(name2[i]<97){
-            name2[i] = name2[i]+32;
-        } 
-     }
-  for(int j=0;j<=strlen(name)-2;j++){
-            sum += name[j];
-  }
-  for(int j=0;j<=strlen(name2)-2;j++){
-            sum2 +=name2[j];
-       
-  }
-if(sum>sum2){
-    printf("1\n");
-}
-else if(sum == sum2){
-    printf("0\n");
-}
-else if(sum<sum2){
-    printf("-1\n");
-}
+    string a, b;
+    cin >> a >> b;
+
+    transform(a.begin(), a.end(), a.begin(), ::tolower);
+    transform(b.begin(), b.end(), b.begin(), ::tolower);
+
+    if (a < b)
+        cout << -1 << endl;
+    else if (a > b)
+        cout << 1 << endl;
+    else
+        cout << 0 << endl;
+
     return 0;
 }
