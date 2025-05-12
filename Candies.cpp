d6bd1a820431fa1f39fabcb41972447388564774
Ayush-Chaurasiya <chaurasiyaayush2006@gmail.com>
2025-05-12 22:35:25 +0530

    Candies

---
+++ Candies.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int n;
+	cin>>n;
+	while(n--) {
+		long long int x;
+		long long val=0;
+		cin>>x;
+		for(int i=2; val<=x; i++) {
+			val = (pow(2,i)-1);
+			if(x%val == 0) {
+				cout<< x/val<<endl;
+				break;
+			}
+		}
+	}
+}
