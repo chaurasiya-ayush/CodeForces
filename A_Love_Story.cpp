3c38c6089783b0c1e63786976f81b181f7634a62
Ayush-Chaurasiya <chaurasiyaayush2006@gmail.com>
2025-07-21 18:25:19 +0530

    Love Story

---
+++ A_Love_Story.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+    string target = "codeforces";
+    string s;
+    cin >> s;
+    int diff = 0;
+    for (int i = 0; i < 10; i++) {
+        if (s[i] != target[i]) diff++;
+    }
+    cout << diff << '\n';
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
