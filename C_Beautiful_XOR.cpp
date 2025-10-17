628a7d7660cf3a5daac6b49834dd1b38d8bcd6bd
Ayush-Chaurasiya <chaurasiyaayush2006@gmail.com>
2025-10-18 00:14:38 +0530

    Question

--- C_Beautiful_XOR.cpp
+++ C_Beautiful_XOR.cpp
@@ -1,30 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+#define ll long long
+const ll mod = 1000000007;
+#define srt(v) sort(v.begin(), v.end());
+#define srt_Dec(v) sort(v.begin(), v.end(), greater<int>());
 
-    int t;
-    cin >> t;
-    while (t--) {
-        long long a, b;
-        cin >> a >> b;
+int solve1()
+{
 
-        if (a == b) {
-            cout << 0 << "\n"; 
-        } 
-        else if (a < b) {
-            cout << -1 << "\n"; 
-        } 
-        else {
-            long long x = a ^ b;
-            if (x <= a) {
-                cout << 1 << "\n"; 
-                cout << x << "\n";
-            } else {
-                cout << -1 << "\n"; 
+    int a, b;
+    cin >> a >> b;
+
+    if (__lg(b) > __lg(a))
+    {
+        cout << -1 << "\n";
+    }
+    else if (a == b)
+        cout << 0 << "\n";
+
+    else if ((__lg(b) == __lg(a)) && (b > a))
+    {
+        cout << 1 << "\n";
+        cout << (a ^ b) << "\n";
+    }
+
+    else if (a > b)
+    {
+        if (__lg(a) > __lg(b))
+        {
+            if ((a & (1 << __lg(b))) == 0)
+            {
+                a = a ^ (1 << __lg(b));
+                cout << 2 << "\n";
+                cout << (1 << __lg(b))<<" ";
+                cout << (a ^ b) << "\n";
             }
+            else
+            {
+                cout << 1 << "\n";
+                cout << (a ^ b) << "\n";
+            }
+        }
+        else
+        {
+            cout << 1 << "\n";
+            cout << (a ^ b) << "\n";
         }
     }
 
+    return 0;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve1();
+    }
     return 0;
 }
