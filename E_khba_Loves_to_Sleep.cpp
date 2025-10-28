ae195a85e755b7993e28dd8bdc07404bd69dc752
Ayush Chaurasiya <chaurasiyaayush2006@gmail.com>
2025-10-28 21:46:54 +0530

    Change

---
+++ E_khba_Loves_to_Sleep.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t; cin >> t;
+    while (t--) {
+        int n, k;
+        long long x;
+        cin >> n >> k >> x;
+        vector<long long> a(n);
+        for (auto &it : a) cin >> it;
+        
+        sort(a.begin(), a.end());
+        set<long long> used(a.begin(), a.end());
+        
+        vector<long long> result;
+        for (long long pos = 0; pos <= x && result.size() < k; pos++) {
+            if (!used.count(pos))
+                result.push_back(pos);
+        }
+
+        for (auto val : result) cout << val << " ";
+        cout << "\n";
+    }
+}
