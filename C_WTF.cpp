733acb2b4743e529fec9376474b123b787966c3f
Ayush Chaurasiya <chaurasiyaayush2006@gmail.com>
2025-10-31 23:36:10 +0530

    Changes

---
+++ C_WTF.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i,n;
+	double a,b=0,maxx=0;
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		cin>>a;
+		b+=a;
+		maxx=max(maxx,b/i);
+	}
+	cout<<maxx;
+}
