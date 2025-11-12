5b47b9c73a2b902e75ca847f2fa4db283d782b7b
Ayush Chaurasiya <chaurasiyaayush2006@gmail.com>
2025-11-12 22:42:47 +0530

    Change

--- A_Souvlaki_VS_Kalamaki.cpp
+++ A_Souvlaki_VS_Kalamaki.cpp
@@ -10,9 +10,6 @@ using namespace std;
 #define yes cout << "YES\n"
 #define no cout << "NO\n"
 #define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
-
-
-
 void solve(){
     int n;
     scan(n);
